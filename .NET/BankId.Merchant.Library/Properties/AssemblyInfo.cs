aacc7c811fa413ea31445908dde2c9a5df07abab
csg <stefan.grigore@maxcode.net>
2019-01-09 16:15:21 +0200

    prepare first public version

---
+++ .NET/BankId.Merchant.Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankId.Merchant.Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankId.Merchant.Library")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: InternalsVisibleTo("BankId.Merchant.Console")]
+[assembly: InternalsVisibleTo("BankId.Merchant.Library.Test")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: InternalsVisibleTo("BankId.Merchant.Library.Tests")]
