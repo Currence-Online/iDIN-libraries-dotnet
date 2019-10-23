43ebb7e770e77dee7e36c13d84b62acaf8e976e5
cristian.mihaita@maxcode.net <cristian.mihaita@maxcode.net>
2019-10-23 14:08:08 +0300

    Add additional library async methods

--- .NET/BankId.Merchant.Library/Messenger.cs
+++ .NET/BankId.Merchant.Library/Messenger.cs
@@ -1,22 +1,9 @@
-﻿using System;
+﻿ using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BankId.Merchant.Library
 {
-    /// <summary>
-    /// IMessenger interface, implemented by <see cref="Messenger"/>.
-    /// </summary>
-    public interface IMessenger
-    {
-        /// <summary>
-        /// Sends the specified message to the given url
-        /// </summary>
-        /// <param name="message">Message to be sent</param>
-        /// <param name="url">Url to send the message</param>
-        /// <returns></returns>
-        string SendMessage(string message, Uri url);
-    }
-
     internal class Messenger : IMessenger
     {
         private readonly ILogger _logger;
@@ -30,9 +17,8 @@ namespace BankId.Merchant.Library
         {
             using (var httpClient = new HttpClient())
             {
-                //httpClient.DefaultRequestHeaders.Add("Content-Type", "text/xml; charset='utf-8'");
                 var task = httpClient.PostAsync(url, new StringContent(message, System.Text.Encoding.UTF8, "text/xml"));
-                
+
                 var result = task.Result;
                 _logger.Log("result status: {0}", result.StatusCode);
                 if (!result.IsSuccessStatusCode)
@@ -42,6 +28,24 @@ namespace BankId.Merchant.Library
                 }
 
                 var content = result.Content.ReadAsStringAsync().Result;
+                return content;
+            }
+        }
+
+        public async Task<string> SendMessageAsync(string message, Uri url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.PostAsync(url, new StringContent(message, System.Text.Encoding.UTF8, "text/xml"));
+                
+                _logger.Log("result status: {0}", result.StatusCode);
+                if (!result.IsSuccessStatusCode)
+                {
+                    _logger.Log("http request failed: {0}", result.StatusCode);
+                    throw new CommunicatorException("http request failed, code=" + result.StatusCode);
+                }
+
+                var content = await result.Content.ReadAsStringAsync();
                 return content;
             }
         }
