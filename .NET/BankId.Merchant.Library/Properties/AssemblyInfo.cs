5859acd92a2c64cc5ca5e1a9db85839bb14daa54
Team Foundation Server Build <TFSBUILD@code40.local>
2020-08-24 17:18:30 +0300

    Version Update

--- .NET/BankId.Merchant.Library/Properties/AssemblyInfo.cs
+++ .NET/BankId.Merchant.Library/Properties/AssemblyInfo.cs
@@ -16,5 +16,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: InternalsVisibleTo("BankId.Merchant.Library.Tests")]
+//necessary in order to allow for unit tests
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
